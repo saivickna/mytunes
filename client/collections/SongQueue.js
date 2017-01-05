68693e6875a7b11c6b85fe0b2703f29f71ceb476
Hack Reactor Students <accounts@hackreactor.com>
2017-01-04 16:08:27 -0800

    Implement all tests but App View tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,33 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function () {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+
+    }, this);
+    this.on('ended', function () {
+      this.removeSong(0);
+      if (this.length !== 0) {
+        this.playFirst();
+      }
+    }, this);
+    this.on('dequeue', function (model) {
+      this.remove(model);
+    }, this);
+  },
+
+  playFirst: function () {
+    // this.at(0).trigger('play');
+    this.at(0).play();
+  },
+
+  removeSong: function (index) {
+    if (this.at(index)) {
+      this.remove(this.at(index));
+    }
+    
   }
 
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,10 +2,16 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.collection.on('add', this.render, this);
+    this.collection.on('remove', this.render, this);
   },
 
   render: function() {
-    return this.$el;
+    return this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
